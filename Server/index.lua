867d349da6e977e85173bceac902cfe8d3ed68ed
Benjamin <Benjaminrowe0@gmail.com>
2021-10-14 07:48:24 +1100

    adding more starting files

--- Server/index.lua
+++ Server/index.lua
@@ -1,19 +1,42 @@
+Package.RequirePackage("nanos-world-sandbox")
+
+
+Player.Unsubscribe("Spawn")
+Package.Unsubscribe("Load")
+Character.Unsubscribe("Respawn")
+
+--Package.RequirePackage("nanos-battlefield-hackspawn")
+
+
+
 -- Called when Players join the server (i.e. Spawn)
+
+--[[
 Player.Subscribe("Spawn", function(new_player)
     -- Spawns a Character at position 0, 0, 0 with default's constructor parameters
-    local new_character = Character(Vector(0, -100, 700))
-    
-    new_character:SetCameraMode(CameraMode.FPSOnly)
-    -- Possess the new Character
-    new_player:Possess(new_character)
-    
-    
+    local post_apocalyptic = Character(Vector(400, 0, 700), Rotator(0, 0, 0), "nanos-world::SK_PostApocalyptic")
+    Package.Log("this also gets called to :) ")
+    post_apocalyptic:SetCameraMode(CameraMode.FPSOnly)
+
+    post_apocalyptic:Subscribe("Death", function(chara, last_damage_taken, last_bone_damaged, damage_reason, hit_from, instigator)
+	
+        post_apocalyptic:Respawn(Vector(400, 0, 100))
+	end)
+
+    new_player:Possess(post_apocalyptic)
+
+end)
+--]]
+Package.Subscribe("Load", function()
+    Package.Log("test")
 end)
 
--- When Player leaves the server, destroy it's Character
-Player.Subscribe("Destroy", function(player)
-    local character = player:GetControlledCharacter()
-    if (character) then
-        character:Destroy()
-    end
+Package.Subscribe("Unload", function()
+    Package.Log("test")
+end)
+
+Character.Subscribe("Respawn", function(character)
+	Package.Log(Character)
+
+    
 end)
