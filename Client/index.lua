b748f8ec35401979ce09c111f434680d01f34c77
Benjamin <Benjaminrowe0@gmail.com>
2021-10-14 09:11:25 +1100

    adding the files for the UI

--- Client/index.lua
+++ Client/index.lua
@@ -1,5 +1,7 @@
 Client.Subscribe("Tick", function(delta_time)   
+    --[[
     for k, player in pairs(Player.GetPairs()) do
         Package.Log(NanosUtils.Dump(player:GetControlledCharacter():GetLocation() ))
     end
+    ]]
 end)
